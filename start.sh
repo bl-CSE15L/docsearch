9bb78f571f6603e8924099132c96f1f0c77cf277
bl-CSE15L <156377155+bl-CSE15L@users.noreply.github.com>
2024-02-09 14:03:28 -0800

    retw

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
-java DocSearchServer.java 0143
+javac Server.java
+javac DocSearchServer.java 
+javac -cp ".;..\libs\junit-4.13.2.jar;..\libs\hamcrest-2.2.jar" DocSearchServer.java
+java DocSearchServer 1430 technical
+java -cp ".;..\libs\junit-4.13.2.jar;..\libs\hamcrest-2.2.jar" org.junit.runner.JUnitCore DocSearchServer
 
